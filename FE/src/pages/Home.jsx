a8bce597c884675a06147829119332eee7d1a0fb
adwaith027 <adwaithh227@gmail.com>
2025-12-23 23:13:35 +0530

    display usename in home

--- FE/src/pages/Home.jsx
+++ FE/src/pages/Home.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import '../styles/Home.css';
 
 export default function Home(){
+    // getItem always returns string. convert it to object
+    const user=localStorage.getItem('user')
+    const userdata=user?JSON.parse(user):{}
+    const username=userdata?.username || ''
+
     return(
         <div className="home-page">
             <div className="home-page__content">
-                <h1 className="home-page__title">Welcome</h1>
+                <h1 className="home-page__title">Welcome, {username}</h1>
 
             </div>
         </div>
